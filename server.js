5a2547f0bf5f328feb6651ec3d56c8b5a8b4bc0f
Jonathan Crider <>
2021-02-16 21:40:33 -0500

    bug fixes

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const { Sequelize, DataTypes } = require('sequelize');
-//lin1 + line 2
-const conn = new Sequelize(process.env.DATABASE_URL || 'postgres:localhost/acme_countrdropy_club');
+const conn = new Sequelize(process.env.DATABASE_URL || 'postgres:localhost/acme_country_club');
 
 const Facility = conn.define('facility', {
   id: {
@@ -30,29 +29,35 @@ const Member = conn.define('member', {
 
 const Booking = conn.define('booking', {
   id: {
+    type: DataTypes.UUID,
     primaryKey: true,
+    defaultValue: DataTypes.UUIDV4
   },
   startTime: {
     type: DataTypes.DATE(),
     allowNull: false,
   },
   endTime: {
+    type: DataTypes.DATE(),
     allowNull: false,
   },
-  fac_name: {
-    type: DataTypes.STRING(100),
-    allowNull: false,
-    unique: true
+});
+
+const Member_booking = conn.define('member_booking', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    allowNull: false
   }
 });
 
 const syncAndSeed = async () => {
-  await conn.sync(options: { force: true });
+  await conn.sync({ force: true });
   const [tennis_court, club_house, larry, moe] = await Promise.all([
-    Facility.create({ name: 'tennis_court' }),
-    Facility.create({ name: 'club_house' }),
-    Member.create({ name: 'larry' }),
-    Member.create({ name: 'moe' }),
+    Facility.create({ fac_name: 'tennis_court' }),
+    Facility.create({ fac_name: 'club_house' }),
+    Member.create({ first_name: 'larry' }),
+    Member.create({ first_name: 'moe' }),
   ]);
 };
 
@@ -60,8 +65,8 @@ const init = async () => {
   try {
     await conn.authenticate();
     await syncAndSeed();
-    const port = process.env.PORT || 1337;
-    app.listen(port, () => console.log(`Listening on port ${port}`));
+    // const port = process.env.PORT || 1337;
+    // app.listen(port, () => console.log(`Listening on port ${port}`));
   }
   catch (ex) {
     console.log(ex);
